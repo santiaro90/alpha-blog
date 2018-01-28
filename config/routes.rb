77294ad1708d59cdae3b68f4e4359e32fb2ff4a0
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-01-28 12:43:24 -0500

    Sign up users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root 'pages#home'
 
-  get "about", to: "pages#about"
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 
+  resources :users, except: [:new]
   resources :articles
 end
