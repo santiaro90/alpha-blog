665a77d5841becf86e88c98d59d2e98606231831
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-01-30 16:01:07 -0500

    Implement login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}"
+
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    redirect_to root_path
+  end
+end
