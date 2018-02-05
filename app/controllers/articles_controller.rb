d0f51a7831b124c3b8ec5b306f5528866ffbbe3a
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-02-05 14:18:01 -0500

    Assign current user to new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first # for the moment, to not get validation errors in UI
+    @article.user = current_user
 
     if @article.save
       flash[:success] = 'Article was successfully created'
