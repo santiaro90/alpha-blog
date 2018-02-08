0a0df9b8485af089548745bf7d8357f20a7b5d6e
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-02-08 16:29:15 -0500

    Test invalid category creation

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,7 +11,19 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
 
     assert_template 'categories/index'
-
     assert_match 'sports', response.body
+  end
+
+  test 'invalid category submission results in failure' do
+    get new_category_path
+
+    assert_template 'categories/new'
+
+    assert_no_difference 'Category.count' do
+      post categories_path, category: { name: '' }
+    end
+
+    assert_template 'categories/new'
+    assert_match(/name can.+t be blank/i, response.body)
   end
 end
