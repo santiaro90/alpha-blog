b7ab87d3f68c1b2a3cd10c5efcdc1fe13eb9c5eb
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-02-09 16:13:24 -0500

    Associate articles and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_and_belongs_to_many :articles
+
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208011741) do
+ActiveRecord::Schema.define(version: 20180209162132) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20180208011741) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
